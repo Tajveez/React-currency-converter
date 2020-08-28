72bb733764f5e108f2da0e23c630156578462ed8
Tajveez <tajveez786@gmail.com>
2020-08-28 23:54:30 +0500

    Code changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Currency Converter</h1>
       <a href="https://youtu.be/XN5elYWiSuw?list=PLZlA0Gpn_vH_NT5zPVp18nGe_W9LqBDQK&t=135">
         Url
       </a>
