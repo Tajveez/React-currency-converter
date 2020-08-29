3b2e0345ac94d5868f011844bc82fd4ea3e38c0c
Tajveez <tajveez786@gmail.com>
2020-08-29 13:16:26 +0500

    converter code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import CurrencyRow from "./components/CurrencyRow";
+
+const baseUrl = "https://api.exchangeratesapi.io/latest";
 
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setfromCurrency] = useState();
+  const [toCurrency, settoCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
+  useEffect(() => {
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        const firstCurrency = Object.keys(data.rates)[0];
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+        setfromCurrency(data.base);
+        settoCurrency(firstCurrency);
+        setExchangeRate(data.rates[firstCurrency]);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      fetch(`${baseUrl}?base=${fromCurrency}&symbols=${toCurrency}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setExchangeRate(data.rates[toCurrency]);
+        });
+    }
+  }, [fromCurrency, toCurrency]);
+
+  const handleFromAmount = (e) => {
+    if (e.target.value != null) {
+      setAmount(e.target.value);
+      setAmountInFromCurrency(true);
+    }
+  };
+
+  const handleToAmount = (e) => {
+    if (e.target.value != null) {
+      setAmount(e.target.value);
+      setAmountInFromCurrency(false);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Currency Converter</h1>
-      <a href="https://youtu.be/XN5elYWiSuw?list=PLZlA0Gpn_vH_NT5zPVp18nGe_W9LqBDQK&t=135">
-        Url
-      </a>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={(e) => setfromCurrency(e.target.value)}
+        amount={fromAmount}
+        onChangeAmount={handleFromAmount}
+      />
+      <div className="equals">=</div>
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={(e) => settoCurrency(e.target.value)}
+        amount={toAmount}
+        onChangeAmount={handleToAmount}
+      />
     </div>
   );
 }
